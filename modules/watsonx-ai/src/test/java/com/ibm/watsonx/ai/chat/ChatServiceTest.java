a68e82496c5ce314e97d35e8172d71b617ec3f25
andreadimaio <dmaio.andrea@gmail.com>
2025-09-24 09:00:46 +0200

    Return the plain content of the message when thinking is enabled, but tags are not present
    Signed-off-by: andreadimaio <dmaio.andrea@gmail.com>

--- modules/watsonx-ai/src/test/java/com/ibm/watsonx/ai/chat/ChatServiceTest.java
+++ modules/watsonx-ai/src/test/java/com/ibm/watsonx/ai/chat/ChatServiceTest.java
@@ -2579,6 +2579,137 @@ public class ChatServiceTest extends AbstractWatsonxTest {
     }
 
     @Test
+    void test_chat_streaming_thinking_without_think_result() throws Exception {
+
+        var httpPort = wireMock.getPort();
+
+        wireMock.stubFor(post("/ml/v1/text/chat_stream?version=%s".formatted(API_VERSION))
+            .withHeader("Authorization", equalTo("Bearer my-super-token"))
+            .withRequestBody(equalToJson("""
+                {
+                    "model_id": "ibm/granite-3-3-8b-instruct",
+                    "project_id": "project-id",
+                    "messages": [
+                        {
+                            "role": "control",
+                            "content": "thinking"
+                        },
+                        {
+                            "role": "user",
+                            "content": [
+                                {
+                                    "type": "text",
+                                    "text": "Translate \\"Hello\\" in Italian"
+                                }
+                            ]
+                        }
+                    ],
+                    "time_limit": 10000
+                }"""))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withChunkedDribbleDelay(159, 200)
+                .withBody(
+                    """
+                        id: 1
+                        event: message
+                        data: {"id":"chatcmpl-5d8c131decbb6978cba5df10267aa3ff","object":"chat.completion.chunk","model":"meta-llama/llama-4-maverick-17b-128e-instruct-fp8", "model_id":"meta-llama/llama-4-maverick-17b-128e-instruct-fp8","model":"meta-llama/llama-4-maverick-17b-128e-instruct-fp8","choices":[{"index":0,"finish_reason":null,"delta":{"role":"assistant","content":""}}],"created":1749736055,"model_version":"4.0.0","created_at":"2025-06-12T13:47:35.541Z","system":{"warnings":[{"message":"This model is a Non-IBM Product governed by a third-party license that may impose use restrictions and other obligations. By using this model you agree to its terms as identified in the following URL.","id":"disclaimer_warning","more_info":"https://dataplatform.cloud.ibm.com/docs/content/wsj/analyze-data/fm-models.html?context=wx"}]}}
+
+                        id: 2
+                        event: message
+                        data: {"id":"chatcmpl-5d8c131decbb6978cba5df10267aa3ff","object":"chat.completion.chunk","model_id":"meta-llama/llama-4-maverick-17b-128e-instruct-fp8","model":"meta-llama/llama-4-maverick-17b-128e-instruct-fp8","choices":[{"index":0,"finish_reason":null,"delta":{"content":"C"}}],"created":1749736055,"model_version":"4.0.0","created_at":"2025-06-12T13:47:35.542Z"}
+
+                        id: 3
+                        event: message
+                        data: {"id":"chatcmpl-5d8c131decbb6978cba5df10267aa3ff","object":"chat.completion.chunk","model_id":"meta-llama/llama-4-maverick-17b-128e-instruct-fp8","model":"meta-llama/llama-4-maverick-17b-128e-instruct-fp8","choices":[{"index":0,"finish_reason":null,"delta":{"content":"iao"}}],"created":1749736055,"model_version":"4.0.0","created_at":"2025-06-12T13:47:35.552Z"}
+
+                        id: 4
+                        event: message
+                        data: {"id":"chatcmpl-5d8c131decbb6978cba5df10267aa3ff","object":"chat.completion.chunk","model_id":"meta-llama/llama-4-maverick-17b-128e-instruct-fp8","model":"meta-llama/llama-4-maverick-17b-128e-instruct-fp8","choices":[{"index":0,"finish_reason":"stop","delta":{"content":""}}],"created":1749736055,"model_version":"4.0.0","created_at":"2025-06-12T13:47:35.563Z"}
+
+                        id: 5
+                        event: message
+                        data: {"id":"chatcmpl-5d8c131decbb6978cba5df10267aa3ff","object":"chat.completion.chunk","model_id":"meta-llama/llama-4-maverick-17b-128e-instruct-fp8","model":"meta-llama/llama-4-maverick-17b-128e-instruct-fp8","choices":[],"created":1749736055,"model_version":"4.0.0","created_at":"2025-06-12T13:47:35.564Z","usage":{"completion_tokens":3,"prompt_tokens":38,"total_tokens":41}}
+                        """)));
+
+        when(mockAuthenticationProvider.asyncToken()).thenReturn(completedFuture("my-super-token"));
+
+        var chatService = ChatService.builder()
+            .authenticationProvider(mockAuthenticationProvider)
+            .modelId("ibm/granite-3-3-8b-instruct")
+            .projectId("project-id")
+            .baseUrl(URI.create("http://localhost:%s".formatted(httpPort)))
+            .build();
+
+        CompletableFuture<ChatResponse> result = new CompletableFuture<>();
+        ChatRequest chatRequest = ChatRequest.builder()
+            .messages(
+                ControlMessage.of("thinking"),
+                UserMessage.text("Translate \"Hello\" in Italian"))
+            .thinking(ExtractionTags.of("think", "response"))
+            .build();
+
+
+        StringBuilder thinkingResponse = new StringBuilder();
+        StringBuilder response = new StringBuilder();
+
+        chatService.chatStreaming(chatRequest, new ChatHandler() {
+
+            @Override
+            public void onPartialResponse(String partialResponse, PartialChatResponse partialChatResponse) {
+                response.append(partialResponse);
+            }
+
+            @Override
+            public void onCompleteResponse(ChatResponse completeResponse) {
+                result.complete(completeResponse);
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                result.completeExceptionally(error);
+            }
+
+            @Override
+            public void onPartialThinking(String partialThinking, PartialChatResponse partialChatResponse) {
+                thinkingResponse.append(partialThinking);
+            }
+
+            @Override
+            public void onPartialToolCall(PartialToolCall partialToolCall) {
+                fail();
+            }
+
+            @Override
+            public void onCompleteToolCall(CompletedToolCall completeToolCall) {
+                fail();
+            }
+        });
+
+        var chatResponse = result.get(3, TimeUnit.SECONDS);
+
+        var EXPECTED_RESPONSE = "Ciao";
+
+        var chatResponseText = chatResponse.getChoices().get(0).getMessage().content();
+        assertFalse(chatResponseText.contains("<think>") && chatResponseText.contains("</think>"));
+        assertFalse(chatResponseText.contains("<response>") && chatResponseText.contains("</response>"));
+        assertTrue(chatResponseText.contains(EXPECTED_RESPONSE));
+
+
+        assertTrue(chatResponse.extractContent().contains(EXPECTED_RESPONSE));
+        assertFalse(chatResponse.extractContent().contains("<response>") && chatResponse.extractContent().contains("</response>"));
+
+        assertNull(chatResponse.extractThinking());
+        assertTrue(response.toString().contains(EXPECTED_RESPONSE));
+
+        var assistantMessage = chatResponse.toAssistantMessage();
+        assertTrue(assistantMessage.content().contains(EXPECTED_RESPONSE));
+        assertFalse(assistantMessage.content().contains("<response>") && assistantMessage.content().contains("</response>"));
+
+        assertNull(assistantMessage.thinking());
+    }
+
+    @Test
     void chat_tool_choice_option_required() throws Exception {
 
         // Watsonx doesn't return "tool_calls" when the tool-choice-option is set to REQUIRED.
