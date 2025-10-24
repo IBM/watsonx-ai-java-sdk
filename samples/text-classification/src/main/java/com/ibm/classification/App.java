6c30212ebffd29ab34982135af56db319a40c097
andreadimaio <dmaio.andrea@gmail.com>
2025-10-24 15:25:51 +0200

    Update samples with last changes
    Signed-off-by: andreadimaio <dmaio.andrea@gmail.com>

--- samples/text-classification/src/main/java/com/ibm/classification/App.java
+++ samples/text-classification/src/main/java/com/ibm/classification/App.java
@@ -2,7 +2,7 @@
  * Copyright IBM Corp. 2025 - 2025
  * SPDX-License-Identifier: Apache-2.0
  */
-package com.ibm.textextraction;
+package com.ibm.classification;
 
 import java.io.File;
 import java.net.URI;
@@ -11,9 +11,8 @@ import java.util.Scanner;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import com.ibm.watsonx.ai.textprocessing.CosReference;
-import com.ibm.watsonx.ai.textprocessing.textextraction.TextExtractionParameters;
-import com.ibm.watsonx.ai.textprocessing.textextraction.TextExtractionParameters.Type;
-import com.ibm.watsonx.ai.textprocessing.textextraction.TextExtractionService;
+import com.ibm.watsonx.ai.textprocessing.textclassification.TextClassificationParameters;
+import com.ibm.watsonx.ai.textprocessing.textclassification.TextClassificationService;
 
 public class App {
 
@@ -33,14 +32,9 @@ public class App {
                 config.getValue("WATSONX_DOCUMENT_REFERENCE_BUCKET", String.class)
             );
 
-            var resultsReference = CosReference.of(
-                config.getValue("WATSONX_RESULTS_REFERENCE_CONNECTION_ID", String.class),
-                config.getValue("WATSONX_RESULTS_REFERENCE_BUCKET", String.class)
-            );
-
             try (Scanner scanner = new Scanner(System.in)) {
 
-                System.out.println("Enter the absolute path to the file to be processed: ");
+                System.out.println("Enter the absolute path to the file to be processed");
                 System.out.print("file: ");
 
                 String inputPath = scanner.nextLine().trim();
@@ -50,33 +44,30 @@ public class App {
 
                 var file = new File(inputPath);
 
-                TextExtractionService textExtractionService = TextExtractionService.builder()
+                TextClassificationService textClassificationService = TextClassificationService.builder()
                     .apiKey(apiKey)
                     .timeout(Duration.ofSeconds(60))
                     .projectId(projectId)
                     .baseUrl(url)
                     .cosUrl(cosUrl)
                     .documentReference(documentReference)
-                    .resultReference(resultsReference)
                     .build();
 
                 Duration timeout = Duration.ofMinutes(5);
 
-                TextExtractionParameters parameters = TextExtractionParameters.builder()
-                    .removeOutputFile(true)
+                TextClassificationParameters parameters = TextClassificationParameters.builder()
                     .removeUploadedFile(true)
-                    .requestedOutputs(Type.MD)
                     .timeout(timeout)
                     .build();
 
-                System.out.println("Starting extraction process with a timeout of %s minute(s)...".formatted(timeout.toMinutes()));
+                System.out.println("Starting classification process with a timeout of %s minute(s)...".formatted(timeout.toMinutes()));
 
                 System.out.println("""
-                    Extracted text:
+                    Classification:
 
                     %s
                     ---------------
-                    """.formatted(textExtractionService.uploadExtractAndFetch(file, parameters)));
+                    """.formatted(textClassificationService.uploadClassifyAndFetch(file, parameters)));
 
                 // Wait for the asynchronous deletion
                 System.out.print("Shutdown application..");
