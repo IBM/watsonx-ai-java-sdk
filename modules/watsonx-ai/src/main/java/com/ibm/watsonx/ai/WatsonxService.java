17c894afc7d0f806761634ac4f66aa3c08f4a211
andreadimaio <dmaio.andrea@gmail.com>
2025-11-06 13:36:16 +0100

    Add Transcribe Audio APIs
    Signed-off-by: andreadimaio <dmaio.andrea@gmail.com>

--- modules/watsonx-ai/src/main/java/com/ibm/watsonx/ai/WatsonxService.java
+++ modules/watsonx-ai/src/main/java/com/ibm/watsonx/ai/WatsonxService.java
@@ -24,6 +24,7 @@ import com.ibm.watsonx.ai.textprocessing.textextraction.TextExtractionService;
 import com.ibm.watsonx.ai.timeseries.TimeSeriesService;
 import com.ibm.watsonx.ai.tokenization.TokenizationService;
 import com.ibm.watsonx.ai.tool.ToolService;
+import com.ibm.watsonx.ai.transcription.TranscriptionRestClient;
 
 /**
  * This class provides common functionality and shared configuration used across various service-specific clients (e.g., {@code ChatService},
@@ -41,6 +42,7 @@ import com.ibm.watsonx.ai.tool.ToolService;
  * @see FoundationModelService
  * @see ToolService
  * @see DetectionService
+ * @see TranscriptionRestClient
  */
 public abstract class WatsonxService {
 
