5bb9b781b867e908f20f73e35261893bb4a959b2
andreadimaio <dmaio.andrea@gmail.com>
2026-08-11 16:08:43 +0200

    Add 502 Bad Gateway to retryable status codes

--- modules/watsonx-ai/src/test/java/com/ibm/watsonx/ai/it/ModelGatewayChatServiceIT.java
+++ modules/watsonx-ai/src/test/java/com/ibm/watsonx/ai/it/ModelGatewayChatServiceIT.java
@@ -54,16 +54,16 @@ import com.ibm.watsonx.ai.chat.model.schema.JsonSchema;
 import com.ibm.watsonx.ai.core.auth.Authenticator;
 import com.ibm.watsonx.ai.core.auth.ibmcloud.IBMCloudAuthenticator;
 import com.ibm.watsonx.ai.core.exception.WatsonxException;
+import com.ibm.watsonx.ai.gateway.chat.ModelGatewayChatParameters;
 import com.ibm.watsonx.ai.gateway.chat.ModelGatewayChatRequest;
 import com.ibm.watsonx.ai.gateway.chat.ModelGatewayChatResponse;
-import com.ibm.watsonx.ai.gateway.chat.ModelGatewayChatParameters;
 import com.ibm.watsonx.ai.gateway.chat.ModelGatewayChatService;
 
-@EnabledIfEnvironmentVariable(named = "WATSONX_API_KEY_GATEWAY", matches = ".+")
+@EnabledIfEnvironmentVariable(named = "WATSONX_API_KEY", matches = ".+")
 @EnabledIfEnvironmentVariable(named = "WATSONX_URL", matches = ".+")
 public class ModelGatewayChatServiceIT {
 
-    static final String API_KEY = System.getenv("WATSONX_API_KEY_GATEWAY");
+    static final String API_KEY = System.getenv("WATSONX_API_KEY");
     static final String URL = System.getenv("WATSONX_URL");
 
     static final Authenticator authentication = IBMCloudAuthenticator.builder()
